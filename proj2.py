a21495da88c37f4b5238fce98439065f79270fc8
Pedro Correia <pedro.correia.105@gmail.com>
2014-12-02 15:05:31 +0000

    comentarios e pedeJogada
    acima descrito

--- proj2.py
+++ proj2.py
@@ -1,82 +1,66 @@
-#variaveis de teste
-#apaga isto depois!!!!!
-t1 = [[1, 2, 4, 8], \
-      [16, 32, 64, 128], \
-      [256, 512, 1024, 2048], \
-      [4096, 8192, 16384, 32768], \
-      0]  
+# grupo 001 Pedro Correia 81002 Rodrigo Rato 81500
 
-t2 = [[2, 2, 0, 0], \
-      [2, 0, 2, 0], \
-      [2, 0, 0, 2], \
-      [0, 0, 2, 2], \
-      0]
+#TAD Coordenada
 
-t3 = [[4, 4, 2, 2], \
-      [4, 4, 4, 4], \
-      [2, 0, 0, 2], \
-      [0, 0, 2, 2], \
-      0]
-
-
-
-
-#Inicio
-jogadas_validas = ('N', 'S', 'W', 'E')
-
-#Construtor
-#cria_coordenada: int x int -> coordenada
-#em que int e um inteiro entre 1 e 4
 def cria_coordenada(x, y):
+    """Construtor: recebe int x, int y em que x e y sao inteiros entre 1 e 4 e devolve o tipo coordenada"""
     if (isinstance(x, int) and x in (1, 2, 3, 4)) and \
        (isinstance(y, int) and y in (1, 2, 3, 4)):
         return (x, y) 
     else:
         raise ValueError("cria_coordenada: argumentos invalidos")
 
-#Selectores
-#coordenada_linha: coordenada -> inteiro
+
 def coordenada_linha(coordenada):
+    """Selector: recebe tipo coordenada e retorna inteiro correspondente a' linha"""
     return coordenada[0]
 
-#coordenada_coluna: coordenada -> inteiro
+
 def coordenada_coluna(coordenada):
+    """Selector: recebe tipo coordenada e retorna inteiro correspondente a' linha"""
     return coordenada[1]
 
-#Reconhecedores
-#e_coordenada: universal -> logico
+
 def e_coordenada(arg):
+    """Reconhecedor: recebe um argumento e retorna valor logico True se for uma coordenada"""
     return (isinstance(arg, tuple) and len(arg) == 2)  and \
            (isinstance(arg[0], int) and arg[0] in (1, 2, 3, 4)) and \
            (isinstance(arg[1], int) and arg[1] in (1, 2, 3, 4))  
 
-#coordenadas_iguais: coordenada x coordenada -> logico
+
 def coordenadas_iguais(c1, c2):
+    """Reconhecedor: recebe duas coordenadas e devolve o valor logico True se forem iguais"""
     return coordenada_linha(c1) == coordenada_linha(c2) and \
            coordenada_coluna(c1) == coordenada_coluna(c2)
 
 
 #TAD Tabuleiro
 
-#Construtor
 def cria_tabuleiro():
-    return [[0, 0, 0, 0], \
-            [0, 0, 0, 0], \
-            [0, 0, 0, 0], \
-            [0, 0, 0, 0], \
-            0]              #resultado
+    """Construtor: nao recebe argumentos e retorna um elemento do tipo tabuleiro com todas as posições com o valor 0 e pontuacao 0"""
+    return [[0, 0, 0, 0] , [0, 0, 0, 0] , [0, 0, 0, 0] , [0, 0, 0, 0] , 0]
 
-#Selectores
+
 def tabuleiro_posicao(tab, coord):
+    """Selector: recebe um tabuleiro e uma coordenada e retorna o valor nessa coordenada"""
     if not e_coordenada(coord):
         raise ValueError("tabuleiro_posicao: argumentos invalidos")
     else:
         return tab[coordenada_linha(coord) - 1][coordenada_coluna(coord) - 1]
 
+
 def tabuleiro_pontuacao(tab):
+    """Selector: recebe um tabuleiro e retorna a pontuacao"""
     return tab[4]
 
+
+def pontuacao_valida(arg):
+    """Reconhcedor: recebe um argumento e retorna o valor logico True se for uma pontuacao valida"""
+    return isinstance(arg, int) and arg >= 0 and arg % 4 == 0
+
+
 def tabuleiro_posicoes_vazias(tab):
+    """Funcao que recebe um tabuleiro e devolve uma lista com as coordenadas onde existem elementos com o valor 0"""
     lista_coordenadas_vazias = []
     for linha in range(1, 5):
         for coluna in range(1, 5):
@@ -85,40 +69,37 @@ def tabuleiro_posicoes_vazias(tab):
                 lista_coordenadas_vazias = lista_coordenadas_vazias + [coord]
     return lista_coordenadas_vazias
 
+
 def tabuleiro_preenche_posicao(tab, coord, num):
+    """Funcao que recebe um tabuleiro, uma coordenada e um numero e retorna o tabuleiro com o valor na coordenada igual ao numero"""
     if not e_coordenada(coord):
         raise ValueError("tabuleiro_preenche_posicao: argumentos invalidos")
     else:
         tab[coordenada_linha(coord) - 1][coordenada_coluna(coord) - 1] = num
         return tab
 
+
 def tabuleiro_actualiza_pontuacao(tab, numero):
+    """Funcao que recebe um tabuleiro e um numero e retorna o tabuleiro com o numero acrescentado a' pontuacao"""
     if not pontuacao_valida(numero):
         raise ValueError("tabuleiro_actualiza_pontuacao: argumentos invalidos")
     else:
         tab[4] = tabuleiro_pontuacao(tab) + numero
         return tab
 
-#junta aos seletores
-def pontuacao_valida(arg):
-    return isinstance(arg, int) and arg >= 0 and arg % 4 == 0
-# junta!!
-
-
-
 
 def tabuleiro_reduz(tab, jogada):
+    """Funcao que recebe um tabuleiro e uma jogada e retorna um novo tabuleiro alterado de acordo com a jogada"""
+    
     def linha_para_lista_tabuleiro(tab, linha):
-        #le os valores de dada linha de um tabuleiro e devolve-os
-        #como uma lista
+        """Funcao que recebe um tabuleiro e um numero correspondente a uma linha e devolve todos os valores da linha como uma lista"""
         lista = []
         for coluna in range(1, 5):
             lista = lista + [tabuleiro_posicao(tab, cria_coordenada(linha, coluna))]
         return lista
     
     def coluna_para_lista_tabuleiro(tab, coluna):
-        #le os valores de dada coluna de um tabuleiro e devolve-os
-        #como uma lista        
+        """Funcao que recebe um tabuleiro e um numero correspondente a uma coluna e devolve todos os valores da coluna como uma lista"""      
         lista = []
         for linha in range(1, 5):
             lista = lista + [tabuleiro_posicao(tab, cria_coordenada(linha, coluna))]
@@ -198,7 +179,7 @@ def tabuleiro_reduz(tab, jogada):
                 lista = move_lista_dir(soma_lista_aux(move_lista_dir(lista), tab))
                 escreve_coluna_tabuleiro(tab, coluna, lista)        
    
-    if not jogada in jogadas_validas:
+    if not jogada in ('N', 'S', 'W', 'E'):
         raise ValueError("tabuleiro_reduz: argumentos invalidos")
     elif jogada == 'N' or jogada == 'S':
         reduz_colunas_aux(tab, jogada)
@@ -264,4 +245,46 @@ def escreve_tabuleiro(tabuleiro):
     print("Pontuacao: " + str(tabuleiro_pontuacao(tabuleiro))) 
 
 def pede_jogada():
+    """Funcao sem parametros que pergunta ao jogador a direcao (N,S,E,W)"""
+    a=input("Introduza uma jogada (N, S, E, W): ")
+    if direcao not in ('N', 'S', 'W', 'E'):
+        print("Jogada invalida.")
+        return pede_jogada()
+    else:
+        return direcao
+
+
+def jogo_2048():
+    tab=cria_tabuleiro
+    escreve_tabuleiro(tab)
+    jogada=pede_jogada
+    escreve_tabuleiro(tabuleiro_reduz(tab,jogada))
     
+    def copia_tabuleiro(tabuleiro):
+        return copiaTabuleiro
+    
+    def preenche_posicao_aleatoria(tabuleiro):
+        return shit
+    
+    if tabuleiro_terminado(tab):
+        print("Jogo acabou, a sua pontuacao é de : ", tabuleiro_pontuacao(tab))
+    else:
+        return
+
+t1 = [[1, 2, 4, 8], \
+      [16, 32, 64, 128], \
+      [256, 512, 1024, 2048], \
+      [4096, 8192, 16384, 32768], 0]
+
+t2 = [[2, 2, 0, 0], \
+      [2, 0, 2, 0], \
+      [2, 0, 0, 2], \
+      [0, 0, 2, 2], \
+      0]
+        
+t3 = [[4, 4, 2, 2], \
+      [4, 4, 4, 4], \
+      [2, 0, 0, 2], \
+      [0, 0, 2, 2], \
+      0]
+
